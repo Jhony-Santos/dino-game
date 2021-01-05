68ce33a1edaf283e78fe4e86fdbbb98512638b3f
Jhonatan Santos <jhony.jpn@gmail.com>
2021-01-05 19:21:51 -0300

    lógica inserida

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-ECHO est  ativado.
+const dino=document.querySelector('.dino')
+
+function espacoPressionado(event){
+    if(event.keyCode ===32){
+       pular()
+    }
+}
+
+function pular(){
+    let position=0
+
+    let upInterval=setInterval(()=>{
+        if(position >= 150){
+            clearInterval(upInterval)
+
+            let downInterval=setInterval(()=>{
+              if(position<=0){
+                  clearInterval(downInterval)
+              }else{
+                  position-=20
+                  dino.style.bottom=position +'px'
+              }
+            },20)
+        }else{
+            position+=20
+            dino.style.bottom=position + 'px'
+        }
+    },20)
+}
+
+
+
+
+
+document.addEventListener('keyup',espacoPressionado)
+
+
+
